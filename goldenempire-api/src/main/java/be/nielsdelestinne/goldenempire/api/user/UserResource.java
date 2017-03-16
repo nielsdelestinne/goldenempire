bebe66e03d77ecb9609468c16df324630367ceab
nielsde <nielsdelestinne@gmail.com>
2017-03-16 22:03:53 +0100

    - Spring-boot-starter-web-services dependency in dependency-management - goldenempire-api dependency on web-services - UserResource

---
+++ goldenempire-api/src/main/java/be/nielsdelestinne/goldenempire/api/user/UserResource.java
@@ -0,0 +1,10 @@
+package be.nielsdelestinne.goldenempire.api.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Niels Delestinne: www.nielsdelestinne.be
+ */
+@RestController
+public class UserResource {
+}
