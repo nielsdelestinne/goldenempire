b30e7dc9da669b55943e214217b43aab74837c67
nielsde <nielsdelestinne@gmail.com>
2017-03-26 17:37:57 +0200

    - Different Application for PRD and for DEV

--- goldenempire-deploy/src/test/java/be/nielsdelestinne/goldenempire/GoldenEmpireApplicationDEV.java
+++ goldenempire-deploy/src/test/java/be/nielsdelestinne/goldenempire/GoldenEmpireApplicationDEV.java
@@ -2,19 +2,21 @@ package be.nielsdelestinne.goldenempire;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.test.context.TestConfiguration;
 
 /**
  * Created by Niels Delestinne: www.nielsdelestinne.be
  */
 @SpringBootApplication
-public class GoldenEmpireApplication {
+@TestConfiguration
+public class GoldenEmpireApplicationDEV {
 
     /**
      * Runs on the Local development Environment
      */
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(GoldenEmpireApplication.class)
+        new SpringApplicationBuilder(GoldenEmpireApplicationDEV.class)
                 .profiles("dev")
                 .run(args);
     }
